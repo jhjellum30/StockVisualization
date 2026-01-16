b6aec622aab7043575c4e1f12994246ee31fece2
jhjellum30 <jay.hjellum@transunion.com>
2026-01-16 16:07:13 -0700

    Update yfinance_demo.py

--- yfinance_demo.py
+++ yfinance_demo.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-ply.style.use('seaborn')
+plt.style.use('seaborn')
 
 ticker=yf.Ticker('msft')
 stockinfo = ticker.info
