08950e225ce33b573b8b4f09a812d4e42cabd035
jhjellum30 <jay.hjellum@transunion.com>
2026-01-16 14:10:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config("Stock Market Visualizer", layout="wide")
 st.title("📈 Stock Market Visualizer")
 
 # Sidebar Controls
-ticker = st.sidebar.text_input("Stock Ticker", "AAPL")
+ticker = st.sidebar.text_input("Stock Ticker", "NVDA")
 start = st.sidebar.date_input("Start Date", pd.to_datetime("2022-01-01"))
 end = st.sidebar.date_input("End Date", pd.to_datetime("today"))
 
